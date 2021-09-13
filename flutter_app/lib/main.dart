c274a678a98aa04483b7a98408c0722153a05bbd
oldman07 <backagain010@gmail.com>
2021-09-13 09:24:47 +0500

    myportfoilio

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,105 +1,159 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false, ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Opacity(
+        opacity: 1,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/bg.jpg"),
+              fit: BoxFit.cover,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 35.0, left: 20),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundImage: AssetImage("images/avatar.png"),
+                    ),
+                    SizedBox(
+                      width: 50,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Basit Ali",
+                          style: TextStyle(fontSize: 30,color: Colors.white, fontFamily: "Code"),
+                        ),
+                        Text(
+                          "Developer",
+                          style: TextStyle(color: Colors.white,fontSize: 15, fontFamily: "Code"),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.school,
+
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 25,
+                          ),
+                          Text(
+                            "BS in SE",
+                            style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.computer_rounded,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 25,
+                          ),
+                          Text(
+                            "Portfolio App",
+                            style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.location_pin,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 25,
+                          ),
+                          Text(
+                            "example@gmail.com",
+                            style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.email,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 25,
+                          ),
+                          Text(
+                            "+92 8369393179",
+                            style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10,),
+                      Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.phone,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 25,
+                          ),
+                          Text(
+                            "Army Public School",
+                            style: TextStyle(color: Colors.white,fontSize: 20, fontFamily: "Code"),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 50,),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(" I am a coder and currently I am a student in University. I teach programming on YouTube. And Last but not least I am a very Accha Baccha.",style: TextStyle(color: Colors.white,fontSize: 22, fontFamily: "Code"),),
+                ),
+                SizedBox(height: 20,),
+                Text("Created By Ali ",style: TextStyle(color: Colors.white, fontFamily: "Code"),)
+              ],
             ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
